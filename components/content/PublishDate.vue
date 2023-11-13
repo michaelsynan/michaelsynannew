5604f88828784c1bfb6a4f8692f37242d13a4554
mikey <mforest@protonmail.com>
2023-11-13 05:24:32 -0500

    style updates

--- components/content/PublishDate.vue
+++ components/content/PublishDate.vue
@@ -15,7 +15,7 @@ function formatDate(date) {
     <p class="m-0 !my-0 text-left text-sm md:text-lg text-zinc-500">
       {{ formatDate(page.createdAt) }}
     </p>
-    <div class="flex mt-2">
+    <div class="flex mt-4">
     <div v-for="(tag, index) in page.tag" :key="index" class="mr-2 bg-primary-dark text-sm py-0.5 px-2.5 shadow rounded-full lowercase text-stone-300 opacity-60">{{ tag }}</div>
     </div>
   </div>
