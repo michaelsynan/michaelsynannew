fcbff3e1ffa87bdf17fdd2a6a81fbd6f9cdb7784
mikey <mforest@protonmail.com>
2023-11-25 11:45:01 -0500

    Fixed a style

--- pages/about.vue
+++ pages/about.vue
@@ -12,7 +12,7 @@
             class="floating-box tracking-wide about2 leading-snug md:leading-normal col-span-2 align-middle sm:col-span-3 xs:col-span-3 md:col-span-2 pl-0 md:pl-4">
             <h2 class="text-2xl md:text-4xl font-bold  pt-0 tracking-wider">A Little About Me</h2>
             <p class="pb-6 text-lg md:text-xl pb-6 text-stone-200 !leading-relaxed tracking-wide font-switzer">
-              I'm Michael, I help create <strong>meaningful online experiences</strong> from the ground up—design to code.
+              I'm Michael, I help create meaningful online experiences from the ground up—design to code.
               I'm a long-time Linux user, philosophy nerd, and open-source advocate.
               My Master’s degree in Urban Affairs and Bachelor’s degree in Psychology inform my approach to understanding
               the human side of technology.
