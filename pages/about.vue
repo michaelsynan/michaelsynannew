f548b310da96f83a3cf059284bbe943c0847bcf8
mikey <mforest@protonmail.com>
2023-12-12 16:59:13 -0500

    added loading image

--- pages/about.vue
+++ pages/about.vue
@@ -4,13 +4,16 @@
       <section role="region" aria-label="About Section">
         <div class="grid grid-cols-3 gap-4 md:gap-8 pb-0 pt-16 md:pt-8">
           <div class="about1 relative col-span-3 pb-1/1 md:col-span-1 drop-shadow-sm">
-            <img class="rounded-sm absolute top-0 object-cover object-center w-full h-full" src="/michael_synan.jpg"
+            <img class="rounded-sm absolute top-0 object-cover object-center w-full h-full" v-if="!isImageLoaded"
+              src="/placeholder1.webp" alt="Loading" />
+            <img class="rounded-sm absolute top-0 object-cover object-center w-full h-full" @load="isImageLoaded = true" v-show="isImageLoaded" src="/michael_synan.webp"
               alt="Profile picture of Michael Synan, front-end engineer and UI/UX designer." />
           </div>
           <div
             class="floating-box tracking-wide about2 col-span-2 align-middle sm:col-span-3 xs:col-span-3 md:col-span-2 pl-0 md:pl-4">
             <h2 class="text-2xl md:text-4xl font-bold  pt-0 tracking-wider">A Little About Me</h2>
-            <p class="pb-6 mr-0 md:mr-10 text-lg md:text-xl pb-6 text-stone-200 leading-relaxed md:leading-loose tracking-wide font-switzer">
+            <p
+              class="pb-6 mr-0 md:mr-10 text-lg md:text-xl pb-6 text-stone-200 leading-relaxed md:leading-loose tracking-wide font-switzer">
               I'm Michael, I help create meaningful online experiences from the ground up—design to code.
               I'm a long-time Linux user, philosophy nerd, and open-source advocate.
               My Master’s degree in Urban Affairs and Bachelor’s degree in Psychology inform my approach to understanding
@@ -29,10 +32,12 @@
 </template>
 
 <script setup>
+import { ref } from 'vue';
 
 definePageMeta({ layout: 'default' });
 
 const runtimeConfig = useRuntimeConfig();
+const isImageLoaded = ref(false);
 
 useHead({
   title: "About",
