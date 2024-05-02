15466c8a533f8359b5fa2ca73b9a0ffb218c8939
mikey <mforest@protonmail.com>
2024-05-02 14:05:30 -0400

    adding some links to the ai section

--- pages/personal-ai-site.vue
+++ pages/personal-ai-site.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="flex flex-col justify-center items-center text-white min-h-screen">
     <div class="flex-grow w-full flex items-center justify-center">
-      <div class="w-full md:w-2/3 lg:w-1/2 mx-auto p-4">
+      <div class="w-full md:w-2/3 lg:w-2/5 mx-auto p-4">
         <transition name="fade-in" mode="out-in">
   <!-- Display when thinking -->
   <div v-if="thinking" class="text-left text-xl tracking-wide w-full">Thinking{{dots}}</div>
@@ -12,7 +12,7 @@
   </div>
 
   <!-- Default message display -->
-  <div v-else key="message" class="text-left text-base md:text-xl tracking-wide w-full leading-9">
+  <div v-else key="message" class="text-left text-base md:text-xl tracking-wide w-full leading-10">
     {{ msg }}
   </div>
 </transition>
@@ -35,14 +35,20 @@
       </div>
       </div>
     </div>
-    <div class="w-full px-4 pb-4 md:pb-10 md:w-2/3 lg:w-1/2 mx-auto relative">
-      <div id="myMessageBox" class="text-stone-600 absolute bottom-full mb-2 left-0 w-full">
-        <div v-if="yourMsg" class="bg-opacity-10 p-4 rounded-lg">You: {{ yourMsg }}</div>
-      </div>
-      <input v-model="inputMessage" @keyup.enter="sendYourMsg"
-             class="w-full p-2 rounded-lg bg-opacity-10 bg-gray-800 placeholder-gray-500 text-white border border-gray-700 focus:ring-2 focus:ring-blue-500 focus:border-transparent"
-             placeholder="Type your message..." />
-    </div>
+    <div class="w-full px-4 pb-4 md:pb-10 md:w-2/3 lg:w-2/5 mx-auto relative">
+  <div id="myMessageBox" class="text-stone-600 absolute bottom-full mb-2 left-0 w-full">
+    <div v-if="yourMsg" class="bg-opacity-10 p-4 rounded-lg">You: {{ yourMsg }}</div>
+  </div>
+  <div class="input-wrapper flex items-center p-2 bg-opacity-10 bg-gray-800 rounded-lg border border-opacity-20 border-stone-600">
+    <input v-model="inputMessage" @keyup.enter="sendYourMsg"
+           class="w-full bg-transparent placeholder-gray-500 text-white border-none"
+           placeholder="Type your message..." />
+    <button @click="sendYourMsg" class="ml-2 bg-blue-500 hover:bg-blue-700 text-white font-bold py-1.5 px-3 rounded opacity-50">
+      Send
+    </button>
+  </div>
+</div>
+
   </div>
 </template>
 
@@ -104,15 +110,15 @@ const sendYourMsg = async () => {
   inputMessage.value = "";
 };
 
-// Initial message change after a few seconds only once on component mount
-onMounted(() => {
-  setTimeout(() => {
-    if (isInitialMessage.value) {
-      msg.value = initialMsg.value;
-      setTimeout(() => isInitialMessage.value = false, 5000);  // Automatically transition from the initial message
-    }
-  }, 2500);
-});
+// // Initial message change after a few seconds only once on component mount
+// onMounted(() => {
+//   setTimeout(() => {
+//     if (isInitialMessage.value) {
+//       msg.value = initialMsg.value;
+//       setTimeout(() => isInitialMessage.value = false, 5000);  // Automatically transition from the initial message
+//     }
+//   }, 2500);
+// });
 
 useHead({
   title: "About",
@@ -130,4 +136,14 @@ useHead({
 .fade-in-enter, .fade-in-leave-to {
   opacity: 0;
 }
+
+.input-wrapper:focus-within {
+  outline: none;
+  box-shadow: 0 0 0 2px #3182ce; /* Focus ring color */
+  border-color: transparent;
+}
+.input-wrapper input:focus {
+  outline: none; /* Removes default focus outline from the input element */
+}
+
 </style>
