00086989df9403d822cfbcdc4149d00314977cf9
mikey <mforest@protonmail.com>
2024-05-15 21:20:48 -0400

    update ai style

--- pages/personal-ai-site.vue
+++ pages/personal-ai-site.vue
@@ -1,15 +1,15 @@
 <template>
   <div class="flex flex-col justify-center items-center text-stone-100 min-h-screen">
     <div class="flex-grow w-full flex items-center justify-center">
-      <div class="w-full md:w-2/3 lg:w-2/5 mx-auto p-4">
-        <div>
-          <select v-model="selectedDemo" @change="onDemoChange" class="bg-stone-900 border border-stone-800 border-opacity-50 shadow text-stone-100 p-2 rounded">
+      <div class="w-full md:w-2/3 lg:w-2/5 mx-auto p-4 relative">
+        <div class="absolute top-0 right-0 md:relative cursor-pointer">
+          <select v-model="selectedDemo" @change="onDemoChange" class="bg-stone-900 border border-stone-800 border-opacity-50 shadow text-stone-100 p-2 rounded ">
             <option value="regular">Regular Chat</option>
             <option value="embeddings">Embeddings Chat</option>
           </select>
         </div>
         <div class="text-left text-xl md:text-2xl tracking-wide w-full !leading-normal border-stone-800 border-opacity-50 border bg-stone-900 p-4 rounded-lg shadow mb-24 md:mb-10 mt-20 md:mt-10">
-          <span class="block text-xl pb-2 font-semibold text-stone-500 tracking-wide flex justify-between items-center">
+          <span class="block text-xl pb-3 font-semibold text-stone-500 tracking-wide flex justify-between items-center">
   Assistant
   <span class="cursor-pointer">
     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
