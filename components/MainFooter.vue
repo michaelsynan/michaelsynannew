61743ff307c192754bf6e46b4d8999ef980ca32b
mikey <mforest@protonmail.com>
2024-09-02 20:04:31 -0400

    style: fixed footer link

--- components/MainFooter.vue
+++ components/MainFooter.vue
@@ -3,9 +3,10 @@
     <div class="mx-5 border-t-2 border-stone-600  w-11/12 sm:w-3/4 md:w-2/3">
       <div class="py-4 text-white justify-between flex w-full">
         <div class="flex items-start group"  aria-label="Get in Contact" title="Contact Form">
-          <a href="https://airtable.com/shrTD2l45wB1qa6Pb"
-            class="inline-block align-text-top text-stone-400 group-hover:text-stone-200 text-base" target="_blank">Send me a message <i-mdi-arrow-top-right-thick class="inline text-stone-400 group-hover:text-stone-200 text-base" />
-          </a>
+<NuxtLink to="/contact"
+          class="inline-block align-text-top text-stone-400 group-hover:text-stone-200 text-base">
+  Send me a message <i-mdi-arrow-top-right-thick class="inline text-stone-400 group-hover:text-stone-200"></i-mdi-arrow-top-right-thick>
+</NuxtLink>
         </div>
         <div><SocialIconGroup small class="pt-0"/></div>
       </div>
